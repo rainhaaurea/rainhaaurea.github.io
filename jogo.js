f9f0bfd151b3bd31c2b17d5ac0dd0179bde46485
rainhaaurea <s05473737@gmail.com>
2024-06-06 16:22:42 -0300

    Add files via upload
    Astronauta

---
+++ jogo.js
@@ -0,0 +1,239 @@
+/* Códidos do Professor José Assis*/*
+
+
+
+
+function py(){
+	document.getElementById("pergunta").src="e.png";
+	
+	}
+	
+function nockout(){
+	 document.getElementById("pergunta").src="e.png";
+
+}
+	function jab(){
+	document.getElementById("pergunta").src="astrol.png";
+    
+	
+	}
+	
+	
+
+function oc(){
+	document.getElementById("ff").src="astrol.png";
+}
+function eu(){
+	document.getElementById("ff").src="e.png";
+}
+function ac(){
+	document.getElementById("ff").src="e.png";
+}
+function px(){
+	document.getElementById("hh").src="e.png";
+}
+function ct(){
+	document.getElementById("hh").src="astrol.png";
+}
+function eq(){
+	document.getElementById("hh").src="e.png";
+}
+function st(){
+	document.getElementById("ab").src="e.png";
+}
+function nt(){
+	document.getElementById("ab").src="e.png";
+}
+function jp (){
+	document.getElementById("ab").src="astrol.png";
+}
+
+function gk(){
+	document.getElementById("p2").src="e.png";
+	
+}
+function po(){
+	document.getElementById("p2").src="e.png";
+}
+function ju(){
+	document.getElementById("p2").src="astrol.png";
+	
+}
+function ca(){
+	document.getElementById("p3").src="e.png";
+	
+}
+function li(){
+	document.getElementById("p3").src="e.png";
+}
+function cl(){
+	document.getElementById("p3").src="astrol.png";
+}
+function um(){
+	document.getElementById("bola").src="e.png";
+}
+function dois(){
+	document.getElementById("bola").src="astrol.png";
+	
+}
+function sete(){
+	document.getElementById("bola").src="e.png";
+}
+function s(){
+	document.getElementById("su").src="e.png";
+	
+}
+function n(){
+	document.getElementById("su").src="e.png";
+	
+}
+function tal(){
+	document.getElementById("su").src="astrol.png";
+}
+function poker(){
+	document.getElementById("ola").src="astrol.png";
+}
+function reis(){
+	document.getElementById("ola").src="e.png";
+}
+function damas(){
+	document.getElementById("ola").src="e.png";
+}
+
+
+///
+function es(){
+	document.getElementById("r1").src="e.png";
+	
+	}
+	
+function ter(){
+	 document.getElementById("r1").src="e.png";
+
+}
+	function soa(){
+	document.getElementById("r1").src="astrol.png";
+    
+	
+	}
+	
+	function su (){
+	document.getElementById("r2").src="e.png";
+	
+	}
+	
+function ly(){
+	 document.getElementById("r2").src="e.png";
+
+}
+	function ela(){
+	document.getElementById("r2").src="astrol.png";
+    
+	
+	}
+	function so(){
+	document.getElementById("r3").src="e.png";
+	
+	}
+	
+function res(){
+	 document.getElementById("r3").src="e.png";
+
+}
+	function ai(){
+	document.getElementById("r3").src="astrol.png";
+    
+	
+	}
+	function faj(){
+	document.getElementById("r4").src="e.png";
+	
+	}
+	
+function xis(){
+	 document.getElementById("r4").src="e.png";
+
+}
+	function kam(){
+	document.getElementById("r4").src="astrol.png";
+    
+	
+	}
+	
+	function ma(){
+	document.getElementById("r5").src="e.png";
+	
+	}
+	
+function ça(){
+	 document.getElementById("r5").src="e.png";
+
+}
+	function fu(){
+	document.getElementById("r5").src="astrol.png";
+    
+	
+	}
+function myau(){
+	document.getElementById("r6").src="e.png";
+	
+	}
+	
+function kiri (){
+	 document.getElementById("r6").src="e.png";
+
+}
+	function zyr(){
+	document.getElementById("r6").src="astrol.png";
+    
+	
+	}
+
+function le(){
+	document.getElementById("r7").src="e.png";
+	
+	}
+	
+function quey(){
+	 document.getElementById("r7").src="e.png";
+
+}
+	function mo(){
+	document.getElementById("r7").src="astrol.png";
+    
+	
+	}
+function ti(){
+	document.getElementById("r8").src="e.png";
+	
+	}
+	
+function bu(){
+	 document.getElementById("r8").src="e.png";
+
+}
+	function ras(){
+	document.getElementById("r8").src="astrol.png";
+    
+	
+	}
+function cão(){
+	document.getElementById("r9").src="e.png";
+	
+	}
+	
+function ka(){
+	document.getElementById("r9").src="e.png";
+
+}
+	function luz(){
+	document.getElementById("r9").src="astrol.png";
+    
+	
+	}
+
+
+
+	
+
+
