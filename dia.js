7dc0298cd2e58af6b11053cf477c54b8d9a4534f
rainhaaurea <s05473737@gmail.com>
2024-06-06 16:18:49 -0300

    Add files via upload
    Sexto dia

---
+++ dia.js
@@ -0,0 +1,56 @@
+/*Códigos do Professor José Assis*/
+
+
+function silva(){
+	if(document.getElementById("zu").checked==true){
+	alert("Sim.Deus disse:Haja luz! E houve luz.");
+	}else{
+		alert("Infelizmente não.");
+	}
+}
+function ar(){
+	if(document.getElementById("be").checked==true){
+	alert("Parabéns! Esaú viva brigando com seu irmão. Não briguem com os seu irmãos.");
+	}else{
+		alert("Infelizmente não.");
+}
+}
+function jogue(){
+	if(document.getElementById("s").checked==true){
+	alert("Parabéns.Um submarino animal!!");
+	}else{
+		alert("Infelizmente não.");
+}
+}
+function aurea(){
+	if(document.getElementById("fk").checked==true){
+	alert("Parabéns!");
+	}else{
+		alert("Infelizmente não.");
+}
+}
+
+function jogar(){
+	if(document.getElementById("b3").checked==true){
+		alert("Parabéns! E o anjo de Deus fechou a boca dos leões.");
+	}else{
+		alert("Infelizmente não.");
+}
+}
+function suely(){
+	if(document.getElementById("3").checked==true){
+	alert("Parabéns! E o Gigante veio ao chão.");
+	}else{
+		alert("Infelizmente não.");
+}
+}
+
+
+
+
+
+
+
+
+
+
