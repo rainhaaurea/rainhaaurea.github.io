fb870ae5005aec9a8e07de4a440c8fb58c20e592
rainhaaurea <s05473737@gmail.com>
2024-06-06 16:13:51 -0300

    Add files via upload
    PÁGINA KIDS

---
+++ k1.js
@@ -0,0 +1,51 @@
+/*Códigos do Professor José Assis*/
+
+function nockout(){
+	document.getElementById("suely").src="aanock.gif";
+	
+
+}
+function jab(){
+	document.getElementById("suely").src="gifvenc.gif";
+
+}
+
+function errada(){
+	document.getElementById("pergunta").src="aanock.gif";
+}
+function certa(){
+	document.getElementById("pergunta").src="gifvenc.gif";
+	
+}
+
+function vdd(){
+	document.getElementById("mundo").src ="gifvenc.gif";
+	
+		
+}
+function sds(){
+	document.getElementById("mundo").src ="aanock.gif";
+	
+}
+function mãe(){
+	document.getElementById("p5").src="aanock.gif";
+	
+}
+function pai(){
+	document.getElementById("p5").src="aanock.gif";
+}
+function caim(){
+		document.getElementById("p5").src="gifvenc.gif";
+	
+}
+
+function marido(){
+	document.getElementById("eva").src="gifvenc.gif";
+	
+}
+function jon(){
+	document.getElementById("eva").src="aanock.gif";
+}
+function ped(){
+	document.getElementById("eva").src="aanock.gif";
+}
