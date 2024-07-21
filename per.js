94290d595cdb195e4c3187a91038a1f150a01d18
rainhaaurea <s05473737@gmail.com>
2024-07-21 16:58:09 -0300

    Add files via upload
    Concurso  do personagem

---
+++ per.js
@@ -0,0 +1,29 @@
+
+    
+	let sim =document.querySelector('#ck')
+	let pontos = 3
+
+function certo(){
+
+	if (sim.value.toUpperCase() == "ARFAXADE"){
+     
+	
+	alert('Parabéns!')
+	
+	}else{
+		alert('Infelizmente não. Tente novamente')
+		pontos-=1
+		console.log(pontos)
+		if (pontos<0){
+			alert('fim de jogo')
+	
+	
+}
+}
+}
+
+
+
+
+ 
+ 
